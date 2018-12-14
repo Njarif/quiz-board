1ab07785fbafc1095fc203252af3a63739ff9e3b
njarif <francismwangi@franciss-MacBook-Pro.local>
2018-12-14 13:06:02 +0300

    Added read me page

--- js/quiz.js
+++ js/quiz.js
@@ -1,13 +1,4 @@
-// $(document).ready(function(){
-//   $('input').each(function(){
-//     $(this).click(function(){
-//       console.log($(this).val());
-//     });
-//   });
-// })
-
 function showSelected() {
-
   var questionOneAnswer = $("input[name=questionOne]:checked").val();
   var questionTwoAnswer = $("input[name=questionTwo]:checked").val();
   var questionThreeAnswer = $("input[name=questionThree]:checked").val();
@@ -16,23 +7,17 @@ function showSelected() {
   var questionSixAnswer = $("input[name=questionSix]:checked").val();
   var questionSevenAnswer = $("input[name=questionSeven]:checked").val();
   var questionEightAnswer = $("input[name=questionEight]:checked").val();
-  if (questionOneAnswer == null || questionTwoAnswer == null || questionThreeAnswer == null || questionFourAnswer == null || questionFiveAnswer == null || questionSixAnswer == null || questionSevenAnswer == null || questionEightAnswer == null || ) {
+  if (questionOneAnswer == null || questionTwoAnswer == null || questionThreeAnswer == null || questionFourAnswer == null || questionFiveAnswer == null || questionSixAnswer == null || questionSevenAnswer == null || questionEightAnswer == null ) {
     console.log('You did not answer all the questions');
     $('.error').fadeIn();
   } else {
     $('.error').fadeOut();
     $('form').fadeOut();
-    //process the user answers
-    // var form = $('.formQuiz');
-    // var values = form.serialize();
-    // console.log(questionOneAnswer);
-    // console.log(questionTwoAnswer);
-    // console.log(questionThreeAnswer);
     var score = 0;
     //check if the answers are correct
     if (questionOneAnswer == "a") {
       //Assign marks to correct answer
-      score = score + 10;
+      score = score + 15;
     }
     //scoring for question 2
     if (questionTwoAnswer == "c") {
@@ -41,27 +26,27 @@ function showSelected() {
     }
     if (questionThreeAnswer == "d") {
       //Assign marks to correct answer
-      score = score + 12;
+      score = score + 15;
     }
     if (questionFourAnswer == "c") {
       //Assign marks to correct answer
-      score = score + 12;
+      score = score + 15;
     }
     if (questionFiveAnswer == "c") {
       //Assign marks to correct answer
-      score = score + 12;
+      score = score + 15;
     }
     if (questionSixAnswer == "d") {
       //Assign marks to correct answer
-      score = score + 12;
+      score = score + 15;
     }
     if (questionSevenAnswer == "c") {
       //Assign marks to correct answer
-      score = score + 12;
+      score = score + 15;
     }
     if (questionEightAnswer == "a") {
       //Assign marks to correct answer
-      score = score + 12;
+      score = score + 15;
     }
 
     console.log(score);
